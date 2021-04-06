f6fd1ce900b1ca51606b5aeeadd304a59c950a83
Fernando R P Marques <frpm1001@gmail.com>
2021-04-06 12:40:06 -0300

    Add SELECT NOBEL, SELECT within SELECT

--- Zoo.sql
+++ Zoo.sql
@@ -114,8 +114,123 @@ SELECT name
    FROM world
 WHERE name NOT LIKE '% %' AND name LIKE '%a%' AND name LIKE '%e%' AND name LIKE '%i%' AND name LIKE '%o%' AND name LIKE '%u%';
 
--- SELECT BASICS =======================================================
--- SELECT BASICS =======================================================
+-- SELECT NOBEL =======================================================
+
+SELECT yr, subject, winner
+  FROM nobel
+ WHERE yr = 1950;
+
+ SELECT winner
+  FROM nobel
+ WHERE yr = 1962
+   AND subject = 'Literature';
+
+SELECT yr, subject
+  FROM nobel
+ WHERE winner = 'Albert Einstein';
+
+SELECT winner
+  FROM nobel
+ WHERE subject = 'Peace' AND yr >= 2000;
+
+SELECT yr, subject, winner
+  FROM nobel
+ WHERE subject = 'Literature' AND yr >= 1980 AND yr <= 1989;
+
+SELECT * FROM nobel
+ WHERE winner IN ('Theodore Roosevelt',
+                  'Woodrow Wilson',
+                  'Jimmy Carter',
+                  'Barack Obama');
+
+SELECT winner FROM nobel
+WHERE winner LIKE 'John%';
+
+SELECT yr, subject, winner FROM nobel
+WHERE (subject = 'Physics' AND yr = 1980) OR (subject = 'Chemistry' AND yr = 1984);
+
+SELECT yr, subject, winner FROM nobel
+WHERE yr = 1980 AND subject <> 'Chemistry' AND subject <> 'Medicine';
+
+SELECT yr, subject, winner FROM nobel
+WHERE (yr < 1910 AND subject = 'Medicine') OR (yr >= 2004 AND subject = 'Literature');
+
+SELECT * FROM nobel
+WHERE winner = 'PETER GRÜNBERG';
+
+SELECT * FROM nobel
+WHERE winner = 'EUGENE O''NEILL';
+
+SELECT winner, yr, subject FROM nobel
+WHERE winner LIKE 'Sir%'
+ORDER BY yr DESC, winner ASC;
+
+SELECT winner, subject
+  FROM nobel
+ WHERE yr=1984
+ ORDER BY 
+ CASE WHEN subject IN ('Physics','Chemistry') THEN 1 ELSE 0
+END,
+subject, winner;
+
+-- SELECT within SELECT ================================================
+
+SELECT name FROM world
+  WHERE population >
+     (SELECT population FROM world
+      WHERE name='Russia');
+
+SELECT name FROM world
+  WHERE continent = 'europe' AND gdp/population >
+     (SELECT gdp/population FROM world
+      WHERE name='United Kingdom');
+
+SELECT name, continent FROM world
+  WHERE continent =
+     (SELECT continent FROM world
+      WHERE name = 'Argentina')
+  OR continent = 
+     (SELECT continent FROM world
+      WHERE name = 'Australia')
+  ORDER BY name;
+
+SELECT name, population FROM world
+  WHERE population >
+     (SELECT population FROM world
+      WHERE name = 'Canada')
+  AND population < 
+     (SELECT population FROM world
+      WHERE name = 'Poland');
+
+SELECT name, CONCAT(CONVERT(INT, ROUND((population/
+    (SELECT population 
+    FROM world 
+    WHERE name = 'Germany')) * 100, 0)), '%') 
+FROM world
+WHERE continent = 'Europe';
+
+SELECT name
+  FROM world
+ WHERE gdp > ALL(SELECT gdp
+                  FROM world
+                  WHERE gdp>0 AND continent = 'Europe');
+
+SELECT continent, name, area FROM world x
+  WHERE area >= ALL
+    (SELECT area FROM world y
+        WHERE y.continent=x.continent
+          AND area>0);
+
+SELECT continent, name FROM world x
+  WHERE name = ALL
+    (SELECT TOP 1 name FROM world y
+        WHERE y.continent=x.continent);
+
+SELECT name, continent FROM world x
+  WHERE population >= ALL
+    (SELECT population * 3 FROM world y
+        WHERE y.continent=x.continent AND population > 0 AND x.name <> y.name);
+
 -- SELECT BASICS =======================================================
 -- SELECT BASICS =======================================================
 -- SELECT BASICS =======================================================
